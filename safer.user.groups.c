982feb8099ae2fa92edc62a70f9477442db1dd68
pbamr <89595466+pbamr@users.noreply.github.com>
2022-04-09 00:01:34 +0200

    Update safer.user.groups.c

--- safer.user.groups.c
+++ safer.user.groups.c
@@ -564,8 +564,7 @@ SYSCALL_DEFINE5(execve,
 				if (strncmp("/usr/libexec/", filename, 13) == 0) break;
 				if (strncmp("/usr/local/", filename, 11) == 0) break;
 				if (strncmp("/usr/share/", filename, 11) == 0) break;
-				if (strncmp("/usr/scripts/", filename, 13) == 0) break;
-
+				
 				if (strncmp("/lib/", filename, 5) == 0) break;
 				if (strncmp("/opt/", filename, 5) == 0) break;
 				if (strncmp("/etc/", filename, 5) == 0) break;
