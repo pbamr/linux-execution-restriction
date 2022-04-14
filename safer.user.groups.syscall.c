e186516c12ae945c5de8fb47c6550c9cbbc3314a
pbamr <89595466+pbamr@users.noreply.github.com>
2022-04-14 00:22:05 +0000

    Update safer.user.groups.syscall.c

--- safer.user.groups.syscall.c
+++ safer.user.groups.syscall.c
@@ -610,9 +610,7 @@ SYSCALL_DEFINE3(execve,
 				if (strncmp("/usr/libexec/", filename, 13) == 0) break;
 				if (strncmp("/usr/local/", filename, 11) == 0) break;
 				if (strncmp("/usr/share/", filename, 11) == 0) break;
-				/* my choice */
-				if (strncmp("/usr/scripts/", filename, 13) == 0) break;
-
+				
 				if (strncmp("/lib/", filename, 5) == 0) break;
 				if (strncmp("/opt/", filename, 5) == 0) break;
 				if (strncmp("/etc/", filename, 5) == 0) break;
