be9655216367d32112f2dab1a9cc52fc58b96a28
pbamr <89595466+pbamr@users.noreply.github.com>
2025-06-12 08:13:53 +0000

    Add files via upload

--- safer.syscall.hash/fs.example.exec.c
+++ safer.syscall.hash/fs.example.exec.c
@@ -1927,9 +1927,14 @@ static int do_execveat_common(int fd, struct filename *filename,
 
 #ifdef add_safer
 	if (allowed_exec(filename->name, argv) == false) {
-		//return -1;
+
+		return -1;
+
+		/*
+		this is not necessary. if retval = -1 then function putname returns, immediately
 		retval = -1;
 		goto out_ret;
+		*/
 	}
 #endif
 
